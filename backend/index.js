05461ad5a249b688727ba4eb786abed5792dbea7
HKV3D <123318331+harshvyas0803@users.noreply.github.com>
2025-02-14 18:43:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ import { authMiddleware } from './middleware/authMiddleware.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://hub-cde3.onrender.com'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

7af52e36cc037c7d77261a80591fdf398cef34d4
Harsh Vyas <harshvyas0803@gmail.com>
2025-02-16 12:29:25 +0530

    Final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBlog from './pages/CreateBlog';
 import Loginpage from './pages/Loginpage';
@@ -8,18 +8,50 @@ import BlogDetail from './pages/BlogDetail';
 import UpdateBlog from './pages/UpdateBlog';
 import 'react-toastify/dist/ReactToastify.css';
 
+// Inline PublicRoute component: if the user is authenticated, redirect to Home.
+const PublicRoute = ({ children }) => {
+  const authToken = localStorage.getItem('authToken');
+  return authToken ? <Navigate to="/" replace /> : children;
+};
+
 const App = () => {
+  const authToken = localStorage.getItem('authToken');
+
   return (
     <div>
-      {/* The content container below will render your pages based on the route */}
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/createblog" element={<CreateBlog />} />
-          <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route path="/updateblog/:id" element={<UpdateBlog />} />
-          <Route path="/login" element={<Loginpage />} />
-          <Route path="/register" element={<Registerpage />} />
+          {authToken ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/createblog" element={<CreateBlog />} />
+              <Route path="/blog/:id" element={<BlogDetail />} />
+              <Route path="/updateblog/:id" element={<UpdateBlog />} />
+              {/* If logged in, any unknown route redirects to Home */}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </>
+          ) : (
+            <>
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute>
+                    <Loginpage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute>
+                    <Registerpage />
+                  </PublicRoute>
+                }
+              />
+              {/* If not logged in, any unknown route redirects to Login */}
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            </>
+          )}
         </Routes>
       </div>
     </div>
